dffbabac5c9a7f8bd475485cc29382fbe6e55bdb
Mahjoub Kouzbour <m_kouz01@uni-muenster.de>
2022-11-24 20:45:26 +0100

    implement import of multiple volumes -mahjoub
    Former-commit-id: 65572c3c2bd3de42d00908da65079996c88212ed

--- Assets/Scripts/ImportVolume/ImportVolumeObject.cs
+++ Assets/Scripts/ImportVolume/ImportVolumeObject.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 using UnityVolumeRendering;
 
@@ -7,34 +9,48 @@ public class ImportVolumeObject : MonoBehaviour
 {   
     IImageFileImporter importer;
 
-    private VolumeRenderedObject obj1;
-    private VolumeRenderedObject obj2;
-
+    private VolumeRenderedObject renderedObject;
+    private List<VolumeRenderedObject> volumeRenderedObjects = new List<VolumeRenderedObject>();
+    Vector3 volumePosition = new Vector3(2.5f, 0.75f, 2.5f);
+    private int counter = 0;
     private bool cycle = false;
+    
     // Start is called before the first frame update
-    void Start()
+    void Start2()
     {
         importer = ImporterFactory.CreateImageFileImporter(ImageFileFormat.NIFTI);
-        VolumeDataset dataset = importer.Import("Assets/DataFiles/Testing/temperature_01132.nii");
-        obj1 = VolumeObjectFactory.CreateObject(dataset);
-        obj1.transform.position = new Vector3(2, 1, 2);
+        VolumeDataset dataset = importer.Import("Assets/DataFiles/Testing/tev_051.nii");
+        renderedObject = VolumeObjectFactory.CreateObject(dataset);
+        renderedObject.transform.position = volumePosition;
+    }
+
+    void Start()
+    {
+        string[] fileEntries = Directory.GetFiles("Assets/DataFiles/Testing/", "*.nii"); 
+        importer = ImporterFactory.CreateImageFileImporter(ImageFileFormat.NIFTI);
+        foreach (string filePath in fileEntries)
+        {
+            VolumeDataset dataset = importer.Import(filePath);
+            VolumeRenderedObject obj = VolumeObjectFactory.CreateObject(dataset);
+            obj.transform.position = volumePosition;
+            volumeRenderedObjects.Add(obj);
+        }
+
+        renderedObject = volumeRenderedObjects[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*if (!cycle) {
-            VolumeDataset dataset = importer.Import("Assets/DataFiles/Testing/temperature_01132.nii");
-            obj2 = VolumeObjectFactory.CreateObject(dataset);
-            Destroy(obj1.gameObject);
-            cycle = true;
-        }
-        else
+        if (cycle)
         {
-            VolumeDataset dataset = importer.Import("Assets/DataFiles/Testing/temperature_01132.nii");
-            obj1 = VolumeObjectFactory.CreateObject(dataset);
-            Destroy(obj2.gameObject);
-            cycle = false;
-        }*/
-    }
+            if (counter == volumeRenderedObjects.Count() - 1)
+                counter = 0;
+            }
+            else
+            {
+                counter++;
+            }
+            renderedObject = volumeRenderedObjects[counter];
+        }
 }
