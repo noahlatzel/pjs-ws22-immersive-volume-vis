b2d489d8478a1ee79bd1c082b10da735494976df
noahlatzel <noahjlatzel@gmail.com>
2022-12-20 13:18:53 +0100

    WIP: Added script to save binary, not working at the moment -Noah
    Former-commit-id: 503d0d4224eee757fc732dbb7861cb93267f8041

---
+++ Assets/Scripts/ImportVolume/StoreBinaries.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityVolumeRendering;
+
+public class StoreBinaries : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get all dataset paths
+        string[] dataSetPaths = Directory.GetDirectories("Assets/Datasets/");
+        
+        foreach (String dataSetPath in dataSetPaths)
+        {
+            // Get all paths to the (four) volume attributes for each dataset
+            string[] volumeAttributePaths = Directory.GetDirectories(dataSetPath).Where(name => !name.EndsWith("_bin")).ToArray();
+
+            foreach (String volumeAttributePath in volumeAttributePaths)
+            {
+                // Set path to store the binary files in separate directory
+                String binaryPath = volumeAttributePaths + "_bin";
+                
+                // Get all paths to .nii files for the given attribute
+                string[] fileEntries = Directory.GetFiles(volumeAttributePath, "*.nii");
+                
+                // Create importer to import all .nii files
+                IImageFileImporter importer = ImporterFactory.CreateImageFileImporter(ImageFileFormat.NIFTI);
+
+                foreach (String file in fileEntries)
+                {
+                    // Convert .nii to dataset and create object from it
+                    VolumeDataset dataset = importer.Import(file);
+                    VolumeRenderedObject obj = VolumeObjectFactory.CreateObject(dataset);
+                    
+                    // Get Texture3D of rendered volume
+                    Texture3D texture = obj.GetComponentInChildren<MeshRenderer>().material.GetTexture("_dataTex");
+                    
+                    // Extract pixel data from texture to save it 
+                    byte[] pixelData = texture.GetPixelData<byte>(0).ToArray();
+                    
+                    // Save pixel data to binary file
+                    File.WriteAllBytes(binaryPath, pixelData);
+                }
+                
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
