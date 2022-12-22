ddd2e59f9e740b400e85082d0180c05dbb96d0d1
Naim Boudaya <nboudaya@ivv5net.wwu.de>
2022-12-22 10:20:51 +0100

    Position and rotation of UI correctly set when trackpad is pressed. -Jasmin, Naim
    Former-commit-id: 62db6bf91c9809faf17470c5c4c1d8962c0f1b54

--- Assets/Scripts/UI/appearInFront.cs
+++ Assets/Scripts/UI/appearInFront.cs
@@ -11,48 +11,35 @@ public class appearInFront : MonoBehaviour
     private GameObject uiCanvas;
     
     private GameObject mainCam;
-    //private GameObject camOffset;
-    
-    //private GameObject leftHand;
-    //private GameObject rightHand;
-    //private XRDirectInteractor leftHandComponent;
-    //private XRDirectInteractor rightHandComponent;
 
     public InputActionProperty showMenu;
-    
-    
+    Quaternion newRot;
+
     // Start is called before the first frame update
     void Start()
     {
         uiCanvas = GameObject.Find("uiCanvas");
      
-        camOffset = GameObject.Find("Camera Offset");
         mainCam = GameObject.Find("Main Camera");
-        
-        
-        leftHand = GameObject.Find("LeftHand");
-        rightHand = GameObject.Find("RightHand");
-        leftHandComponent = leftHand.GetComponent<XRDirectInteractor>();
-        rightHandComponent = rightHand.GetComponent<XRDirectInteractor>();
+
+        newRot = mainCam.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        //if (showMenu.action.WasPressedThisFrame())
-        //{
-            //bool leftHandActive = leftHandComponent.isSelectActive;
-            //bool rightHandActive = rightHandComponent.isSelectActive;
-        
-        
-            Quaternion newRot = new Quaternion(uiCanvas.transform.rotation.x, mainCam.transform.rotation.y, uiCanvas.transform.rotation.z, mainCam.transform.rotation.w);
-            Vector3 newPos = ((newRot * Vector3.forward * 3) + mainCam.transform.position);
-            newPos.y = 1f;
+    {       
+        if (showMenu.action.WasPressedThisFrame())
+        {
+            Quaternion posRot = new Quaternion(uiCanvas.transform.rotation.x, mainCam.transform.rotation.y, uiCanvas.transform.rotation.z, mainCam.transform.rotation.w);
+            Vector3 newPos = ((posRot * Vector3.forward * 1) + mainCam.transform.position);
+            newPos.y = mainCam.transform.position.y - 0.3f;
         
             //Debug.Log(" w:" + mainCam.transform.rotation.w + " x:" + mainCam.transform.rotation.x + " y:" + mainCam.transform.rotation.y + " z:" + mainCam.transform.rotation.z);
 
             uiCanvas.transform.position = newPos;
-            uiCanvas.transform.rotation = Quaternion.RotateTowards(uiCanvas.transform.rotation, newRot, 5f);
-        //}
+            newRot = new Quaternion(uiCanvas.transform.rotation.x, mainCam.transform.rotation.y, uiCanvas.transform.rotation.z, mainCam.transform.rotation.w);
+        }
+        
+        uiCanvas.transform.rotation = Quaternion.RotateTowards(uiCanvas.transform.rotation, newRot, 5f);
     }
 }
