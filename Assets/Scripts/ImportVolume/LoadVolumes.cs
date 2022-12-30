1236950d6ceed9af14712d480f6d8257be2913f2
noahlatzel <noahjlatzel@gmail.com>
2022-12-30 22:28:51 +0100

    Restructured renderer, now using classes -Noah
    Former-commit-id: 94c2ef9e4bb108a29cb8662dcb6b63f306ea4a58

--- Assets/Scripts/ImportVolume/LoadVolumes.cs
+++ Assets/Scripts/ImportVolume/LoadVolumes.cs
@@ -10,19 +10,7 @@ public class LoadVolumes : MonoBehaviour
 {
     [Tooltip("Specify the name of the dataset you want to use. The dataset needs to be stored in Assets/Datasets/")]
     public String datasetName = "Dataset1";
-    
-    [Tooltip("Specify the name of the pressure directory. The directory needs to be stored in Assets/Datasets/<DatasetName>")]
-    public String pressureDirectory = "Pressure";
-    
-    [Tooltip("Specify the name of the temperature directory. The directory needs to be stored in Assets/Datasets/<DatasetName>")]
-    public String temperatureDirectory = "Temperature";
-    
-    [Tooltip("Specify the name of the water directory. The directory needs to be stored in Assets/Datasets/<DatasetName>")]
-    public String waterDirectory = "Water";
-    
-    [Tooltip("Specify the name of the meteorite directory. The directory needs to be stored in Assets/Datasets/<DatasetName>")]
-    public String meteoriteDirectory = "Meteorite";
-    
+
     [Tooltip("Show/Hide pressure volume.")]
     public bool pressure;
     
@@ -34,14 +22,7 @@ public class LoadVolumes : MonoBehaviour
     
     [Tooltip("Show/Hide meteorite volume.")]
     public bool meteorite;
-
-    private Material[] materials = new Material[4];
-    private MeshRenderer[] meshRenderers = new MeshRenderer[4];
-
     
-    // Initialized in Start() with bufferSize
-    // TODO Use Stack for future and Queue for past instead
-    private int currentTimeStep = 0;
     private float timePassed = 0;
     
     [Tooltip("Start/Stop the animation.")]
@@ -51,70 +32,29 @@ public class LoadVolumes : MonoBehaviour
     [SerializeField]
     private int timesPerSecond = 1;
     
-    public int bufferStackSize = 10;
-    public int bufferSize = 10;
-    private LimitedStack<Texture3D> pastStack;
-
-    private Queue<byte[]> binaryBuffer;
-    private bool isReadingBinary;
-    
     [Tooltip("Start/Stop the buffer.")]
     public bool isBuffering;
-
-    // Load the first volume per attribute with the importer guarantee
+    
+    // Manager class
+    private VolumeManager volumeManager;
+    
+    // Load the first volume per attribute with the importer; guarantees
     // correct configuration of Material properties etc.
     // Start is called before the first frame update
     void Start()
     {
-        // Generate array of all directories to use it in for-loop
-        String[] directories = { temperatureDirectory, pressureDirectory, waterDirectory, meteoriteDirectory};
-        String[] volumeAttributeNames = { "Temperature", "Pressure", "Water", "Meteorite"};
-        String datasetPath = "Assets/Datasets/";
-
-        // Create importer
-        IImageFileImporter importer = ImporterFactory.CreateImageFileImporter(ImageFileFormat.NIFTI);
+        // Create manager class
+        volumeManager = new VolumeManager(datasetName);
         
-        // Initialize buffers
-        pastStack = new LimitedStack<Texture3D>(bufferStackSize);
-        binaryBuffer = new Queue<byte[]>();
-        
-        for (int i = 0; i < directories.Length; i++)
-        {
-            // Get first volume
-            String[] volumes = Directory.GetFiles(datasetPath + datasetName + "/" + directories[i], "*.nii");
-
-            if (volumes.Length > 0)
-            {
-                // Import dataset
-                VolumeDataset dataset = importer.Import(volumes[0]);
-                
-                // Create object from dataset
-                VolumeRenderedObject obj = VolumeObjectFactory.CreateObject(dataset);
-                
-                // Transform object and set hierarchy
-                obj.transform.SetParent(transform);
-                obj.transform.rotation = Quaternion.identity;
-                obj.name = volumeAttributeNames[i];
-                
-                // Disable MeshRenderer
-                obj.GetComponentInChildren<MeshRenderer>().enabled = false;
-                
-                // Save each Material property and MeshRenderer for later use
-                materials[i] = obj.GetComponentInChildren<MeshRenderer>().material;
-                meshRenderers[i] = obj.GetComponentInChildren<MeshRenderer>();
-            }
-        }
+        // Render volumes on start 
+        RenderOnStart(volumeManager);
     }
-
-    // TODO Load binaries at runtime and use buffer DropOutStack?
+    
     // Update is called once per frame
     void Update()
     {
         // Update volume visibility according to public variables
-        meshRenderers[0].enabled = pressure;
-        meshRenderers[1].enabled = temperature;
-        meshRenderers[2].enabled = water;
-        meshRenderers[3].enabled = meteorite;
+        volumeManager.SetVisibilities(new []{pressure, temperature, water, meteorite});
         
         // Only execute as often as specified in timesPerSecond
         float dur = 1f / timesPerSecond;
@@ -123,55 +63,51 @@ public class LoadVolumes : MonoBehaviour
         if (timePassed >= dur)
         {
             timePassed -= dur;
-            if (play && binaryBuffer.Count > 0)
+
+            if (play)
             {
-                // Set the current texture to the next texture in the buffer
-                ((Texture3D) materials[0].GetTexture("_DataTex")).SetPixelData(binaryBuffer.Dequeue(), 0);
-                
-                // Increase current time step. Start from beginning if it reached the end.
-                // Get filepath of all volumes 
-                String[] filePaths = Directory.GetFiles("Assets/Datasets/Dataset1/Pressure_bin", "*.bin").Where(filepath => !filepath.EndsWith("sub.bin")).ToArray();
-                currentTimeStep = (currentTimeStep + 1) % filePaths.Length;
+                volumeManager.NextFrame();
+                Debug.Log("Next Frame!");
             }
         }
         
-        if (!isReadingBinary && isBuffering && binaryBuffer.Count < bufferSize)
+        if (!volumeManager.IsReadingBinary() && isBuffering)
         {
             // Start the binary reading thread
-            StartBufferThread("Assets/Datasets/Dataset1/Pressure_bin");
+            //StartBufferThread("Assets/Datasets/Dataset1/Pressure_bin");
+            
+            var t = new Thread(() => volumeManager.BufferNextFrame());
+            t.Start();
         }
     }
-    
-    // Loads the binary from the specified path and stores the loaded byte array in a queue.
-    // Loading the binary has to be separated from the main function because Unity functions classes
-    // may not be called in other threads than the main thread but we want to load our data in a separate 
-    // thread to reduce lag.
-    private void LoadAndStoreBinary(String path)
-    {
-        Debug.Log("Started thread!");
-        
-        // Set the flag to indicate that the reading operation is in progress
-        isReadingBinary = true;
-        
-        // Get filepath of all volumes 
-        String[] filePaths = Directory.GetFiles(path, "*.bin").Where(filepath => !filepath.EndsWith("sub.bin")).ToArray();
-        
-        // Load pixelData from binary file at next position
-        int nextVolumeToBuffer = (currentTimeStep + 1 + binaryBuffer.Count) % filePaths.Length;
-        byte[] pixelData = File.ReadAllBytes(filePaths[nextVolumeToBuffer]);
-        
-        // Store byte array in queue
-        binaryBuffer.Enqueue(pixelData);
-        
-        // Clear the flag to indicate that the reading operation has completed
-        isReadingBinary = false;
-        Debug.Log("Ended thread!");
-    }
 
-    private Thread StartBufferThread(String path) {
-        var t = new Thread(() => LoadAndStoreBinary(path));
-        t.Start();
-        return t;
+    private void RenderOnStart(VolumeManager volumeManagerObject)
+    {
+        // Create importer
+        IImageFileImporter importer = ImporterFactory.CreateImageFileImporter(ImageFileFormat.NIFTI);
+        
+        foreach (VolumeAttribute volumeAttribute in volumeManagerObject.GetVolumeAttributes())
+        {
+            // Import dataset
+            VolumeDataset dataset = importer.Import(volumeAttribute.GetFirstVolumePathForStart());
+                
+            // Create object from dataset
+            VolumeRenderedObject obj = VolumeObjectFactory.CreateObject(dataset);
+                
+            // Transform object and set hierarchy
+            obj.transform.SetParent(transform);
+            obj.transform.rotation = Quaternion.identity;
+            obj.name = volumeAttribute.GetName();
+                
+            // Disable MeshRenderer
+            obj.GetComponentInChildren<MeshRenderer>().enabled = false;
+                
+            // Save each Material property and MeshRenderer for later use
+            volumeAttribute.SetMaterialReference(obj.GetComponentInChildren<MeshRenderer>().material);
+            volumeAttribute.SetMeshRendererReference(obj.GetComponentInChildren<MeshRenderer>());
+        }
+        
+
     }
 }
 
@@ -201,10 +137,157 @@ public class LimitedStack<T>
 
 public class VolumeAttribute
 {
-    private String name;
+    private readonly String name;
     private bool active;
-    private String[] filePaths;
+    private readonly String[] filePaths;
     private Material material;
     private MeshRenderer meshRenderer;
-    private int count;
+    private readonly Queue<byte[]> bufferQueue;
+    private readonly int count;
+    private readonly String originalPath;
+
+    public VolumeAttribute(String path)
+    {
+        name = new DirectoryInfo(path).Name;
+        filePaths = Directory.GetFiles(path + "_bin/", "*.bin")
+            .Where(filePath => !filePath.EndsWith("_sub.bin")).ToArray();
+        count = filePaths.Length;
+        originalPath = path;
+        bufferQueue = new Queue<byte[]>();
+    }
+
+    public String GetVolumePath(int number)
+    {
+        return filePaths[number];
+    }
+
+    public String GetFirstVolumePathForStart()
+    {
+        return Directory.GetFiles(originalPath, "*.nii")[0];
+    }
+
+    public String GetName()
+    {
+        return name;
+    }
+
+    public void SetMaterialReference(Material materialRef)
+    {
+        material = materialRef;
+    }
+
+    public void SetMeshRendererReference(MeshRenderer meshRendererRef)
+    {
+        meshRenderer = meshRendererRef;
+    }
+
+    public void SetVisibility(bool visibility)
+    {
+        meshRenderer.enabled = visibility;
+        active = visibility;
+    }
+
+    public bool IsVisible()
+    {
+        return active;
+    }
+
+    public void NextFrame()
+    {
+        // Set the current texture to the next texture in the buffer
+        ((Texture3D) material.GetTexture("_DataTex")).SetPixelData(bufferQueue.Dequeue(), 0);
+    }
+
+    // Loads the binary from the specified path and stores the loaded byte array in a queue.
+    // Loading the binary has to be separated from the main function because Unity functions classes
+    // may not be called in other threads than the main thread but we want to load our data in a separate 
+    // thread to reduce lag.
+    public void BufferNextFrame(int currentTimeStep)
+    {
+        if (bufferQueue.Count < 5)
+        {
+            // Load pixelData from binary file at next position
+            int nextVolumeToBuffer = (currentTimeStep + 1 + bufferQueue.Count) % count;
+            byte[] pixelData = File.ReadAllBytes(filePaths[nextVolumeToBuffer]);
+        
+            // Store byte array in queue
+            bufferQueue.Enqueue(pixelData);
+        }
+    }
+}
+
+public class VolumeManager
+{
+    private readonly String dataSetPath;
+    private VolumeAttribute[] volumeAttributes;
+    private int currentTimeStep;
+    private bool isReadingBinary;
+
+    public VolumeManager(String dataSetName)
+    {
+        dataSetPath = $"Assets/Datasets/{dataSetName}";
+        AddVolumeAttributes();
+    }
+
+    private void AddVolumeAttributes()
+    {
+        var volumeAttributePaths =
+            Directory.GetDirectories(this.dataSetPath).Where(filePath => !filePath.EndsWith("_bin")).ToArray();
+        volumeAttributes = new VolumeAttribute[volumeAttributePaths.Count()];
+        for (var i = 0; i < volumeAttributes.Length; i++)
+        {
+            volumeAttributes[i] = new VolumeAttribute(volumeAttributePaths[i]);
+        }
+    }
+
+    public VolumeAttribute[] GetVolumeAttributes()
+    {
+        return volumeAttributes;
+    }
+
+    public void SetVisibilities(bool[] visibilities)
+    {
+        for (var i = 0; i < visibilities.Length; i++)
+        {
+            volumeAttributes[i].SetVisibility(visibilities[i]);
+        }
+    }
+
+    public void NextFrame()
+    {
+        bool active = false;
+        foreach (var volumeAttribute in volumeAttributes)
+        {
+            if (volumeAttribute.IsVisible())
+            {
+                volumeAttribute.NextFrame();
+            }
+
+            active = true;
+        }
+
+        if (active)
+        {
+            currentTimeStep++;
+        }
+    }
+
+    public void BufferNextFrame()
+    {
+        // Set the flag to indicate that the reading operation is in progress
+        isReadingBinary = true;
+        
+        foreach (var volumeAttribute in volumeAttributes)
+        {
+            volumeAttribute.BufferNextFrame(currentTimeStep);
+        }
+                            
+        // Clear the flag to indicate that the reading operation has completed
+        isReadingBinary = false;
+    }
+
+    public bool IsReadingBinary()
+    {
+        return isReadingBinary;
+    }
 }
