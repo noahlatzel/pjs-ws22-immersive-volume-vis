c46bee6188750a4c6ae790ec15930d7ffaa5fb41
noahlatzel <noahjlatzel@gmail.com>
2022-12-30 20:35:16 +0100

    Implemented multithreading for binary loading, removed old code -Noah
    Former-commit-id: 121178f76a76f76737b18bfaf500e7199956eddc

--- Assets/Scripts/ImportVolume/LoadVolumes.cs
+++ Assets/Scripts/ImportVolume/LoadVolumes.cs
@@ -1,10 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using UnityEngine;
 using UnityVolumeRendering;
 
@@ -54,11 +52,10 @@ public class LoadVolumes : MonoBehaviour
     private int timesPerSecond = 1;
     
     public int bufferStackSize = 10;
-    public int bufferQueueSize = 10;
+    public int bufferSize = 10;
     private LimitedStack<Texture3D> pastStack;
-    private LimitedQueue<Texture3D> futureQueue;
 
-    private Queue<byte[]> threadQueue;
+    private Queue<byte[]> binaryBuffer;
     private bool isReadingBinary;
     
     [Tooltip("Start/Stop the buffer.")]
@@ -78,9 +75,8 @@ public class LoadVolumes : MonoBehaviour
         IImageFileImporter importer = ImporterFactory.CreateImageFileImporter(ImageFileFormat.NIFTI);
         
         // Initialize buffers
-        futureQueue = new LimitedQueue<Texture3D>(bufferQueueSize);
         pastStack = new LimitedStack<Texture3D>(bufferStackSize);
-        threadQueue = new Queue<byte[]>();
+        binaryBuffer = new Queue<byte[]>();
         
         for (int i = 0; i < directories.Length; i++)
         {
@@ -127,20 +123,22 @@ public class LoadVolumes : MonoBehaviour
         if (timePassed >= dur)
         {
             timePassed -= dur;
-            if (play)
+            if (play && binaryBuffer.Count > 0)
             {
-                //pastStack.Push((Texture3D)materials[0].GetTexture("_DataTex"));
-                //Texture3D nextTexture = futureQueue.Dequeue();
-                //materials[0].SetTexture("_DataTex", nextTexture);
-                ((Texture3D) materials[0].GetTexture("_DataTex")).SetPixelData(threadQueue.Dequeue(), 0);
-                //Debug.Log(futureQueue.GetCurrentTexture());
+                // Set the current texture to the next texture in the buffer
+                ((Texture3D) materials[0].GetTexture("_DataTex")).SetPixelData(binaryBuffer.Dequeue(), 0);
+                
+                // Increase current time step. Start from beginning if it reached the end.
+                // Get filepath of all volumes 
+                String[] filePaths = Directory.GetFiles("Assets/Datasets/Dataset1/Pressure_bin", "*.bin").Where(filepath => !filepath.EndsWith("sub.bin")).ToArray();
+                currentTimeStep = (currentTimeStep + 1) % filePaths.Length;
             }
         }
         
-        if (!isReadingBinary && isBuffering)
+        if (!isReadingBinary && isBuffering && binaryBuffer.Count < bufferSize)
         {
             // Start the binary reading thread
-            StartBufferThread("Assets/Datasets/Dataset1/Pressure_bin/prs_001.bin");
+            StartBufferThread("Assets/Datasets/Dataset1/Pressure_bin");
         }
     }
     
@@ -151,96 +149,29 @@ public class LoadVolumes : MonoBehaviour
     private void LoadAndStoreBinary(String path)
     {
         Debug.Log("Started thread!");
+        
         // Set the flag to indicate that the reading operation is in progress
         isReadingBinary = true;
         
-        // Load pixelData from binary file
-        byte[] pixelData = File.ReadAllBytes(path);
+        // Get filepath of all volumes 
+        String[] filePaths = Directory.GetFiles(path, "*.bin").Where(filepath => !filepath.EndsWith("sub.bin")).ToArray();
+        
+        // Load pixelData from binary file at next position
+        int nextVolumeToBuffer = (currentTimeStep + 1 + binaryBuffer.Count) % filePaths.Length;
+        byte[] pixelData = File.ReadAllBytes(filePaths[nextVolumeToBuffer]);
         
         // Store byte array in queue
-        threadQueue.Enqueue(pixelData);
+        binaryBuffer.Enqueue(pixelData);
         
         // Clear the flag to indicate that the reading operation has completed
         isReadingBinary = false;
         Debug.Log("Ended thread!");
     }
 
-    public Thread StartBufferThread(String path) {
+    private Thread StartBufferThread(String path) {
         var t = new Thread(() => LoadAndStoreBinary(path));
         t.Start();
         return t;
-    }
-    
-    // Loads binaries in original size and in scaled down size.
-    // Usage: - materials[0].SetTexture("_DataTex",LoadBinaryToTexture3D(300, 300, 300,
-    //          "Assets/Datasets/Dataset1/Pressure_bin/prs_098.bin"));
-    //        - materials[0].SetTexture("_DataTex",LoadBinaryToTexture3D(100, 100, 100,
-    //          "Assets/Datasets/Dataset1/Pressure_bin/prs_098.bin"));
-    // TODO Slight offset when rendering the volume, parts from the bottom are rendered at the top, needs fix
-    Texture3D LoadBinaryToTexture3D(int width, int height, int depth, String path)
-    {
-        // Copied from Importer
-        TextureFormat texFormat = SystemInfo.SupportsTextureFormat(TextureFormat.RHalf) ? TextureFormat.RHalf : TextureFormat.RFloat;
-        
-        // Create Texture3D object
-        Texture3D texture = new Texture3D(width, height, depth, texFormat, false);
-        
-        // Load pixelData from binary file
-        byte[] pixelData = File.ReadAllBytes(path);
-        
-        // Set pixel data to loaded binary data with
-        // mipLevel 0 according to implementation in IImageFileImporter.Import
-        texture.SetPixelData(pixelData, 0);
-        
-        // Apply texture to load in GPU
-        texture.Apply();
-        
-        return texture;
-    }
-
-    
-    void LoadFutureBuffer(String path, bool scaled = false)
-    {
-        // Only buffer next texture if buffer not full
-        if (!futureQueue.CheckBufferFull() || true)
-        {
-            // Set the flag to indicate that the buffering operation is in progress
-            isBuffering = true;
-            
-            // Calculate path for binary TODO Later add scaled version
-            String calcPath = path + futureQueue.GetCurrentTexture() + ".bin";
-            
-            // Quick fix TODO delete later
-            calcPath = Directory.GetFiles(path, "*.bin").Where(path => !path.EndsWith("sub.bin")).ToArray()[futureQueue.GetCurrentTexture()];
-            Debug.Log(calcPath);
-            // Load bufferedTexture from binary TODO Later add scaled version
-            Texture3D bufferedTexture = LoadBinaryToTexture3D(300, 300, 300, calcPath);
-        
-            // Increment current texture count before enqueueing so other threads can load the next texture
-            futureQueue.IncrementCurrentTexture();
-            futureQueue.IncrementAlreadyBuffered();
-        
-            // Enqueue bufferedTexture TODO Check order of textures (threading)
-            futureQueue.Enqueue(bufferedTexture);
-            
-            // Clear the flag to indicate that the buffering operation has completed
-            isBuffering = false;
-
-            Debug.Log("Buffer loaded!");
-        }
-    }
-    
-    void LoadFutureBuffer(bool scaled = false)
-    {
-        // Get correct TextureFormat (copied from IImageFileImporter)
-        TextureFormat texFormat = SystemInfo.SupportsTextureFormat(TextureFormat.RHalf) ? TextureFormat.RHalf : TextureFormat.RFloat;
-
-        // Create bufferedTexture from binary TODO Later add scaled version
-        Texture3D bufferedTexture = new Texture3D(300, 300, 300, texFormat, false);
-        bufferedTexture.SetPixelData(threadQueue.Dequeue(), 0);
-
-        Debug.Log("Buffer loaded!");
-        Debug.Log($"Binaries to buffer: {threadQueue.Count}");
     }
 }
 
@@ -267,51 +198,13 @@ public class LimitedStack<T>
     }
 }
 
-public class LimitedQueue<T>
+
+public class VolumeAttribute
 {
-    private T[] items;
-    private int first = 0;
-    private int last = 0;
-    private int currentTexture = 0;
-    private int alreadyBuffered = 0;
-
-    public LimitedQueue(int capacity)
-    {
-        items = new T[capacity];
-    }
-
-    public void Enqueue(T item)
-    {
-        items[last] = item;
-        last = (last + 1) % items.Length;
-        //currentTexture++; TODO review later
-    }
-
-    public T Dequeue()
-    {
-        int temp = first;
-        first = (first + 1) % items.Length;
-        alreadyBuffered--;
-        return items[temp];
-    }
-
-    public int GetCurrentTexture()
-    {
-        return currentTexture;
-    }
-    
-    public void IncrementCurrentTexture()
-    {
-        currentTexture = (currentTexture + 1) % 14;
-    }
-
-    public void IncrementAlreadyBuffered()
-    {
-        alreadyBuffered++;
-    }
-
-    public bool CheckBufferFull()
-    {
-        return alreadyBuffered >= items.Length;
-    }
+    private String name;
+    private bool active;
+    private String[] filePaths;
+    private Material material;
+    private MeshRenderer meshRenderer;
+    private int count;
 }
