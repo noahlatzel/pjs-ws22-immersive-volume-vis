0f6456e4ac9e7224bab7bc28ecacd46628aed8d2
Jasmin Horstknepper <jhorstkn@on.wwu.de>
2023-01-12 12:36:01 +0100

    skybox
    Former-commit-id: e68fb7100a9c9498bf616088db4b8799fab7db69

---
+++ tfredblue.tf
@@ -0,0 +1,1 @@
+{"version":1,"colourPoints":[{"dataValue":0.996052622795105,"colourValue":{"r":1.0,"g":0.02087107114493847,"b":0.0,"a":1.0}},{"dataValue":0.0,"colourValue":{"r":0.0,"g":0.1420271396636963,"b":1.0,"a":1.0}}],"alphaPoints":[{"dataValue":0.0,"alphaValue":0.0}]}
