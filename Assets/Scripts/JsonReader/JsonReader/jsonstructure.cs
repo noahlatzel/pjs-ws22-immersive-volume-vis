74e587fd494f3bc38e708ce3a43588dc2a7603c5
Naim <nboudaya@uni-muenster.de>
2023-01-18 15:44:35 +0100

    Added WIP JSON reader script - naim
    Former-commit-id: 498800e610994391387a792f75d7f6c58a8b33ff

---
+++ Assets/Scripts/JsonReader/JsonReader/jsonstructure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace JsonReader
+{
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Embedding
+    {
+        public List<NVector> nVectors { get; set; }
+        public List<double> eigenvalues { get; set; }
+        public List<string> names { get; set; }
+        public string field { get; set; }
+    }
+
+    public class NVector
+    {
+        public int key { get; set; }
+        public List<List<double>> value { get; set; }
+    }
+
+    public class Root
+    {
+        public string hash { get; set; }
+        public List<Embedding> embeddings { get; set; }
+    }
+
+
+}
