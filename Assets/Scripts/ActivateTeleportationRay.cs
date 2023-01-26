c4ac8f0790811582f15a8f6e5ed4b8227cd6909b
Noah Latzel <nlatzel@uni-muenster.de>
2023-01-26 11:24:34 +0100

    Fixed Teleportation - Noah Jasmin
    Former-commit-id: 7092af2a4035d46b230a71867d795196f24cd609

--- Assets/Scripts/ActivateTeleportationRay.cs
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -8,14 +8,19 @@ public class ActivateTeleportationRay : MonoBehaviour
 {
 	public GameObject leftTeleportation;
 	public GameObject rightTeleportation;
+	public GameObject rightGrab;
+	public GameObject leftGrab;
 	
 	public InputActionProperty leftActivate;
 	public InputActionProperty rightActivate;
+
 	
 	void Update()
 	{
 		bool isInArea = GameObject.Find("InteractionCubeStandalone").GetComponent<InteractionCubeStandalone>().AreHandsNearCube();
 		leftTeleportation.SetActive(!isInArea && leftActivate.action.ReadValue<float>() > 0.1f);
 		rightTeleportation.SetActive(!isInArea && rightActivate.action.ReadValue<float>() > 0.1f);
+		leftGrab.SetActive(!isInArea);
+		rightGrab.SetActive(!isInArea);
 	}
 }
