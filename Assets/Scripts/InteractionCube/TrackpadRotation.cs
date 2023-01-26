834f9e5e922c7622a7c03084fa3763990af52e21
Noah Latzel <nlatzel@uni-muenster.de>
2023-01-26 10:42:55 +0100

    Added trackpad rotation -Jasmin Noah
    Former-commit-id: c69154ba34c6e4e79530d5fb16ca7273b6a40e0c

---
+++ Assets/Scripts/InteractionCube/TrackpadRotation.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TrackpadRotation : MonoBehaviour
+{
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float trackpadX = Input.GetAxis("Trackpad X");
+        Debug.Log(trackpadX);
+        transform.Rotate(trackpadX * speed * Time.deltaTime, 0, 0, Space.Self);
+    }
+}
