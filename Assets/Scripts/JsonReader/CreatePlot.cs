ddb4737f7cab329f1d83a3d453b935bbb934df3a
Naim <nboudaya@uni-muenster.de>
2023-02-02 18:27:21 +0100

    Cleanup and comments - Naim
    Former-commit-id: 55961ed148e59b84e676a2f028023fa6201f19db

--- Assets/Scripts/JsonReader/CreatePlot.cs
+++ Assets/Scripts/JsonReader/CreatePlot.cs
@@ -6,6 +6,15 @@ using UnityEngine.Rendering;
 
 public class CreatePlot : MonoBehaviour
 {
+    [Tooltip("Show/Hide pressure plot.")] public bool pressure;
+
+    [Tooltip("Show/Hide temperature plot.")]
+    public bool temperature;
+
+    [Tooltip("Show/Hide water plot.")] public bool water;
+
+    [Tooltip("Show/Hide meteorite plot.")] public bool meteorite;
+
     //Variable to keep track of the selected graph
     public int selectedGraphIndex = -1;
 
@@ -16,7 +25,9 @@ public class CreatePlot : MonoBehaviour
 
     private void Start()
     {
+        //Access first layer of plotData
         pointsList = Reader.GiveDataList()[0];
+
         // Make sure that the pointsList is not empty
         if (pointsList == null || pointsList.Count == 0)
         {
@@ -24,33 +35,38 @@ public class CreatePlot : MonoBehaviour
             return;
         }
 
-        Debug.Log("CreatePlot:" + pointsList[0][6]);
-
         // Iterate through each list of points
         for (var j = 0; j < pointsList.Count; j++)
         {
+            //Create a new GameObject for every line on the plot
             var newObj = new GameObject();
+
+            //Put the new GameObject under this object in hierarchy
             newObj.transform.SetParent(gameObject.transform);
+
+            //Rename the new GameObject according to the simulation run it belongs to
+            newObj.transform.name = "SimulationRun_" + j;
+
             // Create a new LineRenderer component
             var lineRenderer = newObj.AddComponent<LineRenderer>();
+
+            // Adjust styling of the LineRenderer
             lineRenderer.widthMultiplier = 0.005f;
-            lineRenderer.useWorldSpace = false;
             lineRenderer.shadowCastingMode = ShadowCastingMode.Off;
+
+            //Make LineRenderer's position adjustable
+            lineRenderer.useWorldSpace = false;
 
             // Set the LineRenderer's position count
             lineRenderer.positionCount = pointsList[j].Count;
-            //Iterate through each point in the list
+
+            //Set vertices of the LineRenderer according to plotData for current layer and simulation run
             lineRenderer.SetPositions(pointsList[j].ToArray());
-            // for (int i = 0; i < pointsList[j].Count; i++)
-            // {
-            // //Set the position of each point on the LineRenderer to match the corresponding point in the list
-            //     lineRenderer.SetPosition(i, pointsList[j][i]);
-            // }
         }
     }
 
     //Method to get a specific point value from the graph
-    public Vector2 GetPointValue(int graphIndex, int pointIndex)
+    public Vector3 GetPointValue(int graphIndex, int pointIndex)
     {
         return pointsList[graphIndex][pointIndex];
     }
@@ -62,7 +78,7 @@ public class CreatePlot : MonoBehaviour
     }
 
     //Method that takes the player's position as a parameter and returns the closest point on the selected graph to the player
-    public Vector2 GetClosestPointOnSelectedGraph(Vector3 playerPosition)
+    public Vector3 GetClosestPointOnSelectedGraph(Vector3 playerPosition)
     {
         //Check if a graph has been selected
         if (selectedGraphIndex == -1)
--- Assets/Scripts/JsonReader/Reader.cs
+++ Assets/Scripts/JsonReader/Reader.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
@@ -21,6 +20,10 @@ namespace JsonReader
             return embeddings;
         }
 
+        /*
+         * Returns list of layers (prs, tmp, v01, v02) with every layer containing list of all 7 simulation runs,
+         * with each simulation run containing list of timesteps, with each timestep containing a Vector3 (the relevant data)
+         */
         public static List<List<List<Vector3>>> GiveDataList()
         {
             var embeddings = ReadData();
@@ -43,12 +46,16 @@ namespace JsonReader
             return dataList;
         }
 
+        /*
+         * Converts a list of triplets of doubles (as lists with 3 elements each) to a list of Vector3
+         */
         public static List<Vector3> ConvDoubleToVec(List<List<double>> value)
         {
             var vectorList = new List<Vector3>();
 
-            foreach (var innerList in value) vectorList.Add(new Vector3((float)innerList[0], (float)innerList[1], (float)innerList[2]));
-            
+            foreach (var innerList in value)
+                vectorList.Add(new Vector3((float)innerList[0], (float)innerList[1], (float)innerList[2]));
+
             return vectorList;
         }
     }
