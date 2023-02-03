32bbc07592f3cca996522c9591b206964fb065e7
Naim <nboudaya@uni-muenster.de>
2023-02-03 14:46:04 +0100

    Load all plot data into premade gameObjects - Naim
    Former-commit-id: b008f427e7da7c3438715aa2cb89118e05812071

--- Assets/Scripts/JsonReader/CreatePlot.cs
+++ Assets/Scripts/JsonReader/CreatePlot.cs
@@ -9,25 +9,30 @@ namespace JsonReader
     {
         [Tooltip("Show/Hide pressure plot.")] public bool pressure;
 
-        [Tooltip("Show/Hide temperature plot.")]
-        public bool temperature;
+        [Tooltip("Show/Hide temperature plot.")] public bool temperature;
 
         [Tooltip("Show/Hide water plot.")] public bool water;
 
         [Tooltip("Show/Hide meteorite plot.")] public bool meteorite;
+        
+        [Tooltip("Choose 1D, 2D or 3D plot.")] public int dimensions;
 
         //Variable to keep track of the selected graph
         public int selectedGraphIndex = -1;
 
         private Vector3 playerPosition;
 
+        public string[] layers = new[] { "Pressure", "Temperature", "Water", "Meteorite" };
+
         //List of lists to store multiple sets of points, each representing a separate graph
-        public List<List<Vector3>> pointsList;
+        public List<List<List<Vector3>>> pointsList;
 
         private void Start()
         {
+            dimensions = 3;
+            
             //Access first layer of plotData
-            pointsList = Reader.GiveDataList()[0];
+            pointsList = Reader.GiveDataList();
 
             // Make sure that the pointsList is not empty
             if (pointsList == null || pointsList.Count == 0)
@@ -36,41 +41,61 @@ namespace JsonReader
                 return;
             }
 
-            // Iterate through each list of points
-            for (var j = 0; j < pointsList.Count; j++)
+            for (int i = 0; i < pointsList.Count; i++)
             {
-                //Create a new GameObject for every line on the plot
-                var newObj = new GameObject();
+                // Iterate through each list of points
+                for (var j = 0; j < pointsList[i].Count; j++)
+                {
+                    // //Create a new GameObject for every line on the plot
+                    // var newObj = new GameObject();
+                    //
+                    // //Put the new GameObject under this object in hierarchy
+                    // newObj.transform.SetParent(gameObject.transform);
+                    //
+                    // //Rename the new GameObject according to the simulation run it belongs to
+                    // newObj.transform.name = "SimulationRun_" + j;
+                    //
+                    // // Create a new LineRenderer component
+                    // var lineRenderer = newObj.AddComponent<LineRenderer>();
 
-                //Put the new GameObject under this object in hierarchy
-                newObj.transform.SetParent(gameObject.transform);
+                    var layerObj = gameObject.transform.Find(layers[i]);
 
-                //Rename the new GameObject according to the simulation run it belongs to
-                newObj.transform.name = "SimulationRun_" + j;
+                    var contObj = layerObj.transform.Find("SimRun_" + i + "_" + j);
 
-                // Create a new LineRenderer component
-                var lineRenderer = newObj.AddComponent<LineRenderer>();
+                    var lineRenderer = contObj.GetComponent<LineRenderer>();
 
-                // Adjust styling of the LineRenderer
-                lineRenderer.widthMultiplier = 0.005f;
-                lineRenderer.shadowCastingMode = ShadowCastingMode.Off;
+                    // Adjust styling of the LineRenderer
+                    lineRenderer.widthMultiplier = 0.005f;
+                    lineRenderer.shadowCastingMode = ShadowCastingMode.Off;
 
-                //Make LineRenderer's position adjustable
-                lineRenderer.useWorldSpace = false;
+                    //Make LineRenderer's position adjustable
+                    lineRenderer.useWorldSpace = false;
 
-                // Set the LineRenderer's position count
-                lineRenderer.positionCount = pointsList[j].Count;
+                    // Set the LineRenderer's position count
+                    lineRenderer.positionCount = pointsList[i][j].Count;
 
-                //Set vertices of the LineRenderer according to plotData for current layer and simulation run
-                lineRenderer.SetPositions(pointsList[j].ToArray());
+                    //Set vertices of the LineRenderer according to plotData for current layer and simulation run
+                    lineRenderer.SetPositions(pointsList[i][j].ToArray());
+                }
             }
+
         }
 
-        //Method to get a specific point value from the graph
-        public Vector3 GetPointValue(int graphIndex, int pointIndex)
-        {
-            return pointsList[graphIndex][pointIndex];
-        }
+        // public void SetLineRendererVisibilities(bool[] visibilities)
+        // {
+        //     
+        // }
+        // public List<Vector3> SetPlotData(int dimension)
+        // {
+        //     
+        // }
+        
+        
+        // //Method to get a specific point value from the graph
+        // public Vector3 GetPointValue(int graphIndex, int pointIndex)
+        // {
+        //     return pointsList[graphIndex][pointIndex];
+        // }
 
         //Method to select a graph by its index
         public void SelectGraph(int index)
@@ -78,23 +103,23 @@ namespace JsonReader
             selectedGraphIndex = index;
         }
 
-        //Method that takes the player's position as a parameter and returns the closest point on the selected graph to the player
-        public Vector3 GetClosestPointOnSelectedGraph(Vector3 playerPosition)
-        {
-            //Check if a graph has been selected
-            if (selectedGraphIndex == -1)
-            {
-                Debug.LogError("No graph is selected.");
-                return Vector2.zero;
-            }
-
-            //Use LINQ to calculate the distance between each point on the selected graph and the player's position
-            var distances = pointsList[selectedGraphIndex].Select(p => Vector3.Distance(p, playerPosition));
-            //Find the minimum distance
-            var closestPointIndex = distances.ToList().IndexOf(distances.Min());
-
-            //Return the corresponding point
-            return pointsList[selectedGraphIndex][closestPointIndex];
-        }
+        // //Method that takes the player's position as a parameter and returns the closest point on the selected graph to the player
+        // public Vector3 GetClosestPointOnSelectedGraph(Vector3 playerPosition)
+        // {
+        //     //Check if a graph has been selected
+        //     if (selectedGraphIndex == -1)
+        //     {
+        //         Debug.LogError("No graph is selected.");
+        //         return Vector2.zero;
+        //     }
+        //
+        //     //Use LINQ to calculate the distance between each point on the selected graph and the player's position
+        //     var distances = pointsList[selectedGraphIndex].Select(p => Vector3.Distance(p, playerPosition));
+        //     //Find the minimum distance
+        //     var closestPointIndex = distances.ToList().IndexOf(distances.Min());
+        //
+        //     //Return the corresponding point
+        //     return pointsList[selectedGraphIndex][closestPointIndex];
+        // }
     }
 }
