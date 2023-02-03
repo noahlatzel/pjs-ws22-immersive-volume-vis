588f2860a936017a2c098297e16b7fca2bd93b28
Naim <nboudaya@uni-muenster.de>
2023-02-03 16:54:19 +0100

    WIP UI toggles for simRunVisibilities - Naim
    Former-commit-id: 2832371950f276841e695f0e53ca6e0d9ac00300

--- Assets/Scripts/JsonReader/CreatePlot.cs
+++ Assets/Scripts/JsonReader/CreatePlot.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Rendering;
 
 namespace JsonReader
@@ -9,47 +9,61 @@ namespace JsonReader
     {
         [Tooltip("Show/Hide pressure plot.")] public bool pressure;
 
-        [Tooltip("Show/Hide temperature plot.")] public bool temperature;
+        [Tooltip("Show/Hide temperature plot.")]
+        public bool temperature;
 
         [Tooltip("Show/Hide water plot.")] public bool water;
 
         [Tooltip("Show/Hide meteorite plot.")] public bool meteorite;
-        
+
         [Tooltip("Choose 1D, 2D or 3D plot.")] public int dimensions;
 
-        [Tooltip("Show/Hide simulation run 0 plot.")] public bool simRun0Vis;
-        
-        [Tooltip("Show/Hide simulation run 1 plot.")] public bool simRun1Vis;
-        
-        [Tooltip("Show/Hide simulation run 2 plot.")] public bool simRun2Vis;
-        
-        [Tooltip("Show/Hide simulation run 3 plot.")] public bool simRun3Vis;
-        
-        [Tooltip("Show/Hide simulation run 4 plot.")] public bool simRun4Vis;
-        
-        [Tooltip("Show/Hide simulation run 5 plot.")] public bool simRun5Vis;
-        
-        [Tooltip("Show/Hide simulation run 6 plot.")] public bool simRun6Vis;
+        [Tooltip("Show/Hide simulation run 0 plot.")]
+        public bool simRun0Vis;
+
+        [Tooltip("Show/Hide simulation run 1 plot.")]
+        public bool simRun1Vis;
+
+        [Tooltip("Show/Hide simulation run 2 plot.")]
+        public bool simRun2Vis;
+
+        [Tooltip("Show/Hide simulation run 3 plot.")]
+        public bool simRun3Vis;
+
+        [Tooltip("Show/Hide simulation run 4 plot.")]
+        public bool simRun4Vis;
+
+        [Tooltip("Show/Hide simulation run 5 plot.")]
+        public bool simRun5Vis;
+
+        [Tooltip("Show/Hide simulation run 6 plot.")]
+        public bool simRun6Vis;
 
         //Variable to keep track of the selected graph
         public int selectedGraphIndex = -1;
 
+        public UnityEvent testEvent;
+
+        public string[] layers = { "Pressure", "Temperature", "Water", "Meteorite" };
+
+        private bool[] layerVisibilities = { true, true, true, true };
+
         private Vector3 playerPosition;
 
-        public string[] layers = new[] { "Pressure", "Temperature", "Water", "Meteorite" };
 
         //List of lists to store multiple sets of points, each representing a separate graph
         public List<List<List<Vector3>>> pointsList;
+        private bool[] simRunVisibilities = { true, true, true, true, true, true, true };
 
         private void Start()
         {
             dimensions = 3;
 
-            bool[] layerVisibilities = new[] {pressure, temperature, water, meteorite };
+            layerVisibilities = new[] { pressure, temperature, water, meteorite };
 
-            bool[] simRunVisibilities = new[]
+            simRunVisibilities = new[]
                 { simRun0Vis, simRun1Vis, simRun2Vis, simRun3Vis, simRun4Vis, simRun5Vis, simRun6Vis };
-            
+
             //Access first layer of plotData
             pointsList = Reader.GiveDataList();
 
@@ -60,10 +74,10 @@ namespace JsonReader
                 return;
             }
 
-            for (int i = 0; i < pointsList.Count; i++)
+            for (var i = 0; i < pointsList.Count; i++)
             {
                 var layerObj = gameObject.transform.Find(layers[i]);
-                
+
                 // Iterate through each list of points
                 for (var j = 0; j < pointsList[i].Count; j++)
                 {
@@ -97,38 +111,45 @@ namespace JsonReader
                     lineRenderer.SetPositions(pointsList[i][j].ToArray());
                 }
             }
+
             SetVisibilities(layerVisibilities, simRunVisibilities);
         }
 
-        public void SetVisibilities(bool[] layerVisibilities, bool[] simRunVisibilities)
+        public void SetVisibilities(bool[] layerVisibilitiesArr, bool[] simRunVisibilitiesArr)
         {
-            if ((layerVisibilities.Length != 4) || (simRunVisibilities.Length != 7))
+            if (layerVisibilitiesArr.Length != 4 || simRunVisibilitiesArr.Length != 7)
             {
                 Debug.LogError("CreatePlot.SetVisibilities: Incorrect length at parameter arrays");
                 return;
             }
 
-            for (int i = 0; i < 4; i++)
+            for (var i = 0; i < 4; i++)
             {
                 var layerObj = gameObject.transform.Find(layers[i]).gameObject;
-                
-                for (int j = 0; j < 7; j++)
+
+                for (var j = 0; j < 7; j++)
                 {
                     var simRunObj = layerObj.transform.Find("SimRun_" + i + "_" + j).gameObject;
-                    
-                    simRunObj.SetActive(simRunVisibilities[j]);
+
+                    simRunObj.SetActive(simRunVisibilitiesArr[j]);
                 }
-                
-                layerObj.SetActive(layerVisibilities[i]);
+
+                layerObj.SetActive(layerVisibilitiesArr[i]);
             }
         }
-        
+
+        public void SetVisibilityOfSingleRun(int runNum, bool val)
+        {
+            simRunVisibilities[runNum] = val;
+            SetVisibilities(layerVisibilities, simRunVisibilities);
+        }
+
         // public List<Vector3> SetPlotData(int dimension)
         // {
         //     
         // }
-        
-        
+
+
         // //Method to get a specific point value from the graph
         // public Vector3 GetPointValue(int graphIndex, int pointIndex)
         // {
@@ -140,6 +161,12 @@ namespace JsonReader
         {
             selectedGraphIndex = index;
         }
+
+        public void testBool(bool test)
+        {
+            Debug.Log(test);
+        }
+
 
         // //Method that takes the player's position as a parameter and returns the closest point on the selected graph to the player
         // public Vector3 GetClosestPointOnSelectedGraph(Vector3 playerPosition)
