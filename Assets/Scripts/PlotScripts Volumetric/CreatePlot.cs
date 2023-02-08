1a25200a6872e0f9920a61611d577064c38c9a45
Naim <nboudaya@uni-muenster.de>
2023-02-08 13:19:51 +0100

    Converted lineRenderers to volumetric Line strips, WIP - Naim
    Former-commit-id: 722f7cd39ee0474b360b2c94dc6205efa510f39b

---
+++ Assets/Scripts/PlotScripts Volumetric/CreatePlot.cs
@@ -0,0 +1,175 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace PlotScripts_Volumetric
+{
+    public class CreatePlot : MonoBehaviour
+    {
+        [Tooltip("Choose 1D, 2D or 3D plot.")] public int dimensions;
+
+        // //Variable to keep track of the selected graph
+        // public int selectedGraphIndex = -1;
+
+        public bool[] simRunVisibilities = { true, true, true, true, true, true, true };
+        public bool[] layerVisibilities = { true, false, false, false };
+
+        //Names of all the layers as array
+        private readonly string[] layers = { "Pressure", "Temperature", "Water", "Meteorite" };
+
+        private Vector3 playerPosition;
+
+        //List of lists to store multiple sets of points, each representing a separate graph
+        public List<List<List<Vector3>>> pointsList;
+
+        private void Start()
+        {
+            dimensions = 2;
+
+            // MakeVisArr();
+
+            //Access first layer of plotData
+            pointsList = GivePlotData(dimensions);
+
+            // Make sure that the pointsList is not empty
+            if (pointsList == null || pointsList.Count == 0)
+            {
+                Debug.LogError("pointsList is empty");
+                return;
+            }
+
+            for (var i = 0; i < pointsList.Count; i++)
+            {
+                var layerObj = gameObject.transform.Find(layers[i]);
+
+                // Iterate through each list of points
+                for (var j = 0; j < pointsList[i].Count; j++)
+                {
+                    // //Create a new GameObject for every line on the plot
+                    // var newObj = new GameObject();
+                    //
+                    // //Put the new GameObject under this object in hierarchy
+                    // newObj.transform.SetParent(gameObject.transform);
+                    //
+                    // //Rename the new GameObject according to the simulation run it belongs to
+                    // newObj.transform.name = "SimulationRun_" + j;
+                    //
+                    // // Create a new LineRenderer component
+                    // var lineRenderer = newObj.AddComponent<LineRenderer>();
+
+                    var contObj = layerObj.transform.Find("SimRun_" + i + "_" + j);
+
+                    var lineRenderer = contObj.GetComponent<VolumetricLines.VolumetricLineStripBehavior>();
+
+                    // Adjust styling of the LineRenderer
+                    lineRenderer.LineWidth = 0.005f;
+
+                    //Set vertices of the LineRenderer according to plotData for current layer and simulation run
+                    lineRenderer.UpdateLineVertices(pointsList[i][j].ToArray());
+                }
+            }
+
+            SetVisibilities();
+        }
+
+        public void SetVisibilities(bool[] layerVisibilitiesArr, bool[] simRunVisibilitiesArr)
+        {
+            if (layerVisibilitiesArr.Length != 4 || simRunVisibilitiesArr.Length != 7)
+            {
+                Debug.LogError("CreatePlot.SetVisibilities: Incorrect length at parameter arrays");
+                return;
+            }
+
+            layerVisibilities = layerVisibilitiesArr;
+            simRunVisibilities = simRunVisibilitiesArr;
+            
+            SetVisibilities();
+        }
+
+        public void SetVisibilities()
+        {
+            for (var i = 0; i < 4; i++)
+            {
+                var layerObj = gameObject.transform.Find(layers[i]).gameObject;
+
+                for (var j = 0; j < 7; j++)
+                {
+                    var simRunObj = layerObj.transform.Find("SimRun_" + i + "_" + j).gameObject;
+
+                    simRunObj.SetActive(simRunVisibilities[j]);
+                }
+
+                layerObj.SetActive(layerVisibilities[i]);
+            }
+        }
+
+        public void SetVisibilityOfSingleRun(int runNum, bool val)
+        {
+            simRunVisibilities[runNum] = val;
+
+            SetVisibilities();
+            //SetVisibilities(layerVisibilities, simRunVisibilities);
+        }
+
+        // public void MakeVisArr()
+        // {
+        //     layerVisibilities = new[] { pressure, temperature, water, meteorite };
+        //     
+        //     simRunVisibilities = new[]
+        //         { simRun0Vis, simRun1Vis, simRun2Vis, simRun3Vis, simRun4Vis, simRun5Vis, simRun6Vis };
+        // }
+
+        public void MakeVisArr(bool[] layerVisibilitiesArr, bool[] simRunVisibilitiesArr)
+        {
+            if (layerVisibilitiesArr.Length != 4 || simRunVisibilitiesArr.Length != 7)
+            {
+                Debug.LogError("CreatePlot.MakeVisArr: Incorrect length at parameter arrays");
+                return;
+            }
+
+            layerVisibilities = layerVisibilitiesArr;
+
+            simRunVisibilities = simRunVisibilitiesArr;
+        }
+
+        public List<List<List<Vector3>>> GivePlotData(int dimension)
+        {
+            List<List<List<Vector3>>> plotData = Reader.GiveDataList(dimension);
+
+            return plotData;
+        }
+
+
+        // //Method to get a specific point value from the graph
+        // public Vector3 GetPointValue(int graphIndex, int pointIndex)
+        // {
+        //     return pointsList[graphIndex][pointIndex];
+        // }
+
+        // //Method to select a graph by its index
+        // public void SelectGraph(int index)
+        // {
+        //     selectedGraphIndex = index;
+        // }
+
+
+        // //Method that takes the player's position as a parameter and returns the closest point on the selected graph to the player
+        // public Vector3 GetClosestPointOnSelectedGraph(Vector3 playerPosition)
+        // {
+        //     //Check if a graph has been selected
+        //     if (selectedGraphIndex == -1)
+        //     {
+        //         Debug.LogError("No graph is selected.");
+        //         return Vector2.zero;
+        //     }
+        //
+        //     //Use LINQ to calculate the distance between each point on the selected graph and the player's position
+        //     var distances = pointsList[selectedGraphIndex].Select(p => Vector3.Distance(p, playerPosition));
+        //     //Find the minimum distance
+        //     var closestPointIndex = distances.ToList().IndexOf(distances.Min());
+        //
+        //     //Return the corresponding point
+        //     return pointsList[selectedGraphIndex][closestPointIndex];
+        // }
+    }
+}
---
+++ Assets/Scripts/PlotScripts Volumetric/Reader.cs
@@ -0,0 +1,101 @@
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace PlotScripts_Volumetric
+{
+    internal class Reader : MonoBehaviour
+    {
+        private static List<Embedding> ReadData()
+        {
+            const string filePath = @"./Assets/Scripts/PlotScripts Volumetric/plot1.json";
+
+            var jsonContent = File.ReadAllText(filePath);
+
+            var plotData = JsonConvert.DeserializeObject<Root>(jsonContent);
+
+            var embeddings = plotData.embeddings;
+
+            return embeddings;
+        }
+
+        /*
+         * Returns list of layers (prs, tmp, v01, v02) with every layer containing list of all 7 simulation runs,
+         * with each simulation run containing list of timesteps, with each timestep containing a Vector3 (the relevant data)
+         */
+        public static List<List<List<Vector3>>> GiveDataList(int dimension)
+        {
+            var embeddings = ReadData();
+
+            var dataList = new List<List<List<Vector3>>>();
+
+            foreach (var t in embeddings)
+            {
+                var vecList = new List<List<Vector3>>();
+                foreach (var nVectors in t.nVectors)
+                {
+                    var currVecList = PreprocessList(nVectors.value, dimension);
+                    
+                    Debug.Log(currVecList[0].x + ", " + currVecList[0].y + ", " + currVecList[0].z);
+
+                    vecList.Add(currVecList);
+                }
+
+                dataList.Add(vecList);
+            }
+
+            return dataList;
+        }
+
+        /*
+         * Converts a list of triplets of doubles (as lists with 3 elements each) to a list of Vector3
+         */
+        public static List<Vector3> ConvDoubleToVec(List<List<double>> value)
+        {
+            var vectorList = new List<Vector3>();
+
+            foreach (var innerList in value)
+                vectorList.Add(new Vector3((float)innerList[0], (float)innerList[1], (float)innerList[2]));
+
+            return vectorList;
+        }
+
+        public static List<Vector3> ChangeDimension(List<Vector3> valList, int dimension)
+        {
+            var outList = new List<Vector3>();
+            
+             for (int i = 0; i < valList.Count; i++)
+            {
+                switch (dimension)
+                {
+                    case 1:
+                        // Debug.Log("Case 1");
+                        outList.Add(new Vector3(valList[i].x, 0, 0));
+                        break;
+                    case 2:
+                        // Debug.Log("Case 2");
+                        outList.Add(new Vector3(valList[i].x, valList[i].y, 0));
+                        break;
+                    case 3:
+                        // Debug.Log("Case 3");
+                        outList.Add(new Vector3(valList[i].x, valList[i].y, valList[i].z));
+                        break;
+                    default:
+                        Debug.LogError("Reader.ChangeDimension: Invalid dimension");
+                        break;
+                }
+            }
+
+            return outList;
+        }
+
+        public static List<Vector3> PreprocessList(List<List<double>> value, int dimension)
+        {
+            var vectorList = ConvDoubleToVec(value);
+            vectorList = ChangeDimension(vectorList, dimension);
+
+            return vectorList;
+        }
+    }
+}
