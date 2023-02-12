a9c81f84778abdd19c65b60cc3b736317542181b
Naim <nboudaya@uni-muenster.de>
2023-02-12 22:03:09 +0100

    Approptiate options get disabled when volumes disabled, first displayed volume can be selected via dropdown - Naim
    Former-commit-id: 748cda7bf88a706df6ad5923e288702bd66ed3d8

--- Assets/Scripts/PlotScripts Volumetric/EnableVols.cs
+++ Assets/Scripts/PlotScripts Volumetric/EnableVols.cs
@@ -7,86 +7,83 @@ namespace PlotScripts_Volumetric
     public class EnableVols : MonoBehaviour
     {
         public GameObject runSelection;
-        private TMP_Dropdown runSelectionDropdown;
 
         public GameObject layerSelection;
-        private TMP_Dropdown layerSelectionDropdown;
 
         public GameObject volumeTransformer;
-        private GameObject[] volumes;
-    
+        public bool[] volumeVisibility;
+        private TMP_Dropdown layerSelectionDropdown;
+        private TMP_Dropdown runSelectionDropdown;
+
+        public GameObject VolumeTurn; 
+        public GameObject Volume1Desc;
+        public GameObject Volume1Drop;
+        public GameObject Volume2Desc;
+        public GameObject Volume2Drop;
+        
         private Toggle thisToggle;
+
+        private GameObject[] volumes;
+
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
             volumes = new GameObject[volumeTransformer.transform.childCount];
-            
-            Debug.Log("Anzahl an Volumenobjekten: " + volumes.Length);
 
-            for (int i = 0; i < volumes.Length; i++)
-            {
-                volumes[i] = volumeTransformer.transform.GetChild(i).gameObject;
-            }
-            
-            Debug.Log("Name von Volumen 3: " + volumes[3].name);
-            
+            // Debug.Log("Anzahl an Volumenobjekten: " + volumes.Length);
+
+            for (var i = 0; i < volumes.Length; i++) volumes[i] = volumeTransformer.transform.GetChild(i).gameObject;
+
+            // Debug.Log("Name von Volumen 3: " + volumes[3].name);
+
             runSelectionDropdown = runSelection.GetComponent<TMP_Dropdown>();
-            
+
             layerSelectionDropdown = layerSelection.GetComponent<TMP_Dropdown>();
-            
+
+            volumeVisibility = new bool[volumes.Length];
+
+            for (var i = 0; i < volumeVisibility.Length; i++) volumeVisibility[i] = false;
+
             thisToggle = gameObject.GetComponent<Toggle>();
         }
 
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
-        
         }
 
         public void activateVolumes()
         {
             volumeTransformer.transform.position = new Vector3(0f, 6.5f, 20f);
             volumeTransformer.transform.localScale = new Vector3(20f, 20f, 20f);
-            
+
             foreach (var volume in volumes)
-            {
                 if (volume.activeInHierarchy)
                 {
                     var thisLV = volume.GetComponent<LoadVolumes>();
                     thisLV.meteorite = false;
                     thisLV.pressure = false;
                     thisLV.temperature = false;
-                    thisLV.water = false;   
+                    thisLV.water = false;
                 }
-            }
+
+            for (var i = 0; i < volumeVisibility.Length; i++) volumeVisibility[i] = false;
+            
             if (thisToggle.isOn)
             {
-                int currSelectedRun = runSelectionDropdown.value;
-
-                int currSelectedLayer = layerSelectionDropdown.value;
-
-                if (volumes[currSelectedRun].activeInHierarchy)
-                {
-                    var thisLV = volumes[currSelectedRun].GetComponent<LoadVolumes>();
-                    switch (currSelectedLayer)
-                    {
-                        case 0:
-                            thisLV.pressure = true;
-                            break;
-                        case 1:
-                            thisLV.temperature = true;
-                            break;
-                        case 2:
-                            thisLV.water = true;
-                            break;
-                        case 3:
-                            thisLV.meteorite = true;
-                            break;
-                        default:
-                            Debug.Log("EnableVols.activateVolumes: Invalid Value");
-                            break;                        
-                    }
-                }
+                VolumeTurn.SetActive(true);
+                Volume1Desc.SetActive(true);
+                Volume1Drop.SetActive(true);
+                Volume2Desc.SetActive(true);
+                Volume2Drop.SetActive(true);
+            }
+            else
+            {
+                VolumeTurn.SetActive(false);
+                Volume1Desc.SetActive(false);
+                Volume1Drop.SetActive(false);
+                Volume2Desc.SetActive(false);
+                Volume2Drop.SetActive(false);
             }
         }
     }
