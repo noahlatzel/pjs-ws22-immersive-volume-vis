e121f4a86a4b62b76c2520867b4b6554aa2d7ebc
Naim <nboudaya@uni-muenster.de>
2023-02-12 16:41:54 +0100

    Volumes can be enabeld and disabled via button on UI, only enables selected layer of volume and selected run - Naim
    Former-commit-id: a159ad3d83c61dd2b259afaab4f4f64be88770d6

---
+++ Assets/Scripts/PlotScripts Volumetric/EnableVols.cs
@@ -0,0 +1,90 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PlotScripts_Volumetric
+{
+    public class EnableVols : MonoBehaviour
+    {
+        public GameObject runSelection;
+        private TMP_Dropdown runSelectionDropdown;
+
+        public GameObject layerSelection;
+        private TMP_Dropdown layerSelectionDropdown;
+
+        public GameObject volumeTransformer;
+        private GameObject[] volumes;
+    
+        private Toggle thisToggle;
+        // Start is called before the first frame update
+        void Start()
+        {
+            volumes = new GameObject[volumeTransformer.transform.childCount];
+            
+            Debug.Log("Anzahl an Volumenobjekten: " + volumes.Length);
+
+            for (int i = 0; i < volumes.Length; i++)
+            {
+                volumes[i] = volumeTransformer.transform.GetChild(i).gameObject;
+            }
+            
+            Debug.Log("Name von Volumen 3: " + volumes[3].name);
+            
+            runSelectionDropdown = runSelection.GetComponent<TMP_Dropdown>();
+            
+            layerSelectionDropdown = layerSelection.GetComponent<TMP_Dropdown>();
+            
+            thisToggle = gameObject.GetComponent<Toggle>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public void activateVolumes()
+        {
+            foreach (var volume in volumes)
+            {
+                if (volume.activeInHierarchy)
+                {
+                    var thisLV = volume.GetComponent<LoadVolumes>();
+                    thisLV.meteorite = false;
+                    thisLV.pressure = false;
+                    thisLV.temperature = false;
+                    thisLV.water = false;   
+                }
+            }
+            if (thisToggle.isOn)
+            {
+                int currSelectedRun = runSelectionDropdown.value;
+
+                int currSelectedLayer = layerSelectionDropdown.value;
+
+                if (volumes[currSelectedRun].activeInHierarchy)
+                {
+                    var thisLV = volumes[currSelectedRun].GetComponent<LoadVolumes>();
+                    switch (currSelectedLayer)
+                    {
+                        case 0:
+                            thisLV.pressure = true;
+                            break;
+                        case 1:
+                            thisLV.temperature = true;
+                            break;
+                        case 2:
+                            thisLV.water = true;
+                            break;
+                        case 3:
+                            thisLV.meteorite = true;
+                            break;
+                        default:
+                            Debug.Log("EnableVols.activateVolumes: Invalid Value");
+                            break;                        
+                    }
+                }
+            }
+        }
+    }
+}
