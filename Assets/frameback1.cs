07b61e3011d3571082ca1fcccf0b3b39c84f565c
Jasmin_Horstknepper <e_mail42@uni-muenster.de>
2023-03-07 17:12:11 +0100

    Changes UI skript -Jasmin
    Former-commit-id: 88b415fa6dea5185fc4db30b10a47f0c58e4094a

---
+++ Assets/frameback1.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class frameback1 : MonoBehaviour
+{
+    public bool playing;
+
+    public GameObject volume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume = GameObject.Find("RenderedVolume");
+        playing = volume.GetComponent<LoadVolumes>().play;
+    }
+
+    public void SkipFrame(InputAction.CallbackContext context)
+    {
+        if (playing)
+        {
+            volume.GetComponent<LoadVolumes>().timesPerSecond--;
+        }
+        else
+        {
+            volume.GetComponent<LoadVolumes>().volumeManager.PreviousFrame();
+        }
+    }
+}
