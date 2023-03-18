9f8cd83cebd79db9f43c4ca46f3ff597ffa9e852
Noah Johann Latzel <nlatzel@uni-muenster.de>
2023-03-18 22:15:40 +0100

    ChangeScene fixed -Noah
    Former-commit-id: fb9e735fdb6c656e1d9a9bded646e8fbe575d796

---
+++ Assets/Scripts/UI/SimulationControlUI/SelectVolumeDataset.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using TMPro;
+using UnityEngine;
+
+namespace UI.SimulationControlUI
+{
+    public class SelectVolumeDataset : MonoBehaviour
+    {
+        public GameObject volume;
+        private string[] dataSetPaths;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // Get all dataset paths
+            dataSetPaths = Directory.GetDirectories("Assets/Datasets/");
+        
+            // Set dropdown options
+            foreach (var t in dataSetPaths)
+            {
+                GetComponent<TMP_Dropdown>().options.Add(
+                    new TMP_Dropdown.OptionData(Path.GetFileName(t)));
+            }
+        
+            GetComponent<TMP_Dropdown>().onValueChanged.AddListener(delegate
+            {
+                DropDownValueChanged(GetComponent<TMP_Dropdown>().value);
+            });
+        }
+
+        void DropDownValueChanged(int value)
+        {
+            volume.GetComponent<LoadVolumes>().volumeManager.SetDataset(dataSetPaths[value]);
+        }
+    
+    }
+}
