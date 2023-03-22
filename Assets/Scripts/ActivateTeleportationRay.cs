80645cc882f15d44ea33fa7761e1da940e745114
Naim <nboudaya@uni-muenster.de>
2023-03-22 16:06:38 +0100

    ActivateTPRay eingefuegt und verbessert
    Former-commit-id: 739bb4cc1ca14d027a6ff4b3fc5c1cdcb43e862b

--- Assets/Scripts/ActivateTeleportationRay.cs
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -17,36 +17,45 @@ public class ActivateTeleportationRay : MonoBehaviour
 
 	public InputActionProperty leftCancel;
 	public InputActionProperty rightCancel;
-
-	private InteractionCubeStandalone interactionCube;
-	private bool interactionCubeExists = false;
 	
-	void Start()
-	{
-		if (GameObject.Find("InteractionCubeStandalone") != null)
-		{
-			if (GameObject.Find("InteractionCubeStandalone").GetComponent<InteractionCubeStandalone>() != null)
-			{
-				interactionCube = GameObject.Find("InteractionCubeStandalone")
-					.GetComponent<InteractionCubeStandalone>();
-				interactionCubeExists = true;
-			}
-		}
-		
-		
-	}
+	public XRRayInteractor leftRay;
+	public XRRayInteractor rightRay;
+
+	// private InteractionCubeStandalone interactionCube;
+	// private bool interactionCubeExists = false;
+	
+	// void Start()
+	// {
+	// 	if (GameObject.Find("InteractionCubeStandalone") != null)
+	// 	{
+	// 		if (GameObject.Find("InteractionCubeStandalone").GetComponent<InteractionCubeStandalone>() != null)
+	// 		{
+	// 			interactionCube = GameObject.Find("InteractionCubeStandalone")
+	// 				.GetComponent<InteractionCubeStandalone>();
+	// 			interactionCubeExists = true;
+	// 		}
+	// 	}
+	// 	
+	// 	
+	// }
 
 	void Update()
 	{
-		bool isInArea = false;
-		
-		if (interactionCubeExists)
-		{
-			isInArea = interactionCube.AreHandsNearCube();
-		}
-		leftTeleportation.SetActive(!isInArea && /*leftActivate.action.ReadValue<float>() > 0.1f &&*/ leftCancel.action.ReadValue<float>() == 0);
-		rightTeleportation.SetActive(!isInArea && /*rightActivate.action.ReadValue<float>() > 0.1f &&*/ rightCancel.action.ReadValue<float>() == 0);
+		// bool isInArea = false;
+		//
+		// if (interactionCubeExists)
+		// {
+		// 	isInArea = interactionCube.AreHandsNearCube();
+		// }
+		// leftTeleportation.SetActive(!isInArea && /*leftActivate.action.ReadValue<float>() > 0.1f &&*/ leftCancel.action.ReadValue<float>() == 0);
+		// rightTeleportation.SetActive(!isInArea && /*rightActivate.action.ReadValue<float>() > 0.1f &&*/ rightCancel.action.ReadValue<float>() == 0);
 		//leftGrab.SetActive(!isInArea);
 		//rightGrab.SetActive(!isInArea);
+		
+		bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
+		leftTeleportation.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
+			
+		bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid);
+		rightTeleportation.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
 	}
 }
